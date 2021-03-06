bc907d9de3792963d6b11642dc9af09532132318
Peachcii <cpeachcii@gmail.com>
2021-03-06 22:03:14 +0700

    New => Connecting to API

--- src/main/java/th/ac/ku/atm/AtmApplication.java
+++ src/main/java/th/ac/ku/atm/AtmApplication.java
@@ -1,7 +1,9 @@
 package th.ac.ku.atm;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 import static org.springframework.boot.SpringApplication.run;
 
@@ -9,7 +11,13 @@ import static org.springframework.boot.SpringApplication.run;
 public class AtmApplication {
 
 	public static void main(String[] args) {
+
 		run(AtmApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
 	}
 
 }
--- src/main/java/th/ac/ku/atm/service/BankAccountService.java
+++ src/main/java/th/ac/ku/atm/service/BankAccountService.java
@@ -2,30 +2,33 @@ package th.ac.ku.atm.service;
 
 import org.springframework.stereotype.Service;
 import th.ac.ku.atm.model.BankAccount;
-import th.ac.ku.atm.model.Customer;
-
-import java.util.ArrayList;
+import org.springframework.http.*;
+import org.springframework.web.client.RestTemplate;
+import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class BankAccountService {
 
-    private ArrayList<BankAccount> bankAccountsList = new ArrayList<>();
+    private RestTemplate restTemplate;
 
-    public void createAccount(BankAccount bankAccount){
-        BankAccount storedBankAccount = findBankAccount(bankAccount.getId());
-        if(storedBankAccount == null){
-            bankAccountsList.add(bankAccount);
-        }
-
+    public BankAccountService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
     }
 
-    public List<BankAccount> getAccount(){
 
-        return new ArrayList<>(bankAccountsList);
+    public List<BankAccount> getCustomerBankAccount(int customerId) {
+        String url = "http://localhost:8091/api/bankaccount/customer/" +
+                customerId;
+        ResponseEntity<BankAccount[]> response =
+                restTemplate.getForEntity(url, BankAccount[].class);
+
+        BankAccount[] accounts = response.getBody();
+
+        return Arrays.asList(accounts);
     }
 
-    public BankAccount findBankAccount(int id) {
+    public BankAccount findBankAccount(int id, BankAccount[] bankAccountsList) {
         for (BankAccount account : bankAccountsList) {
             if (account.getId() == id)
                 return account;
